48cf6d99720a1bd69a70a311ebb685d6bc178e4d
remon <remosamir443@gmail.com>
2025-03-30 16:05:41 -0700

    first upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Landing from "./Website/Landing/Landing";
-
+import Login from "./Website/Auth/Register/Login";
 function App() {
   return (
     <div className="App">
       <Routes>
-    <Route path="/" element={<Landing/>}/>
+        <Route path="/" element={<Landing />} />
+        <Route path="/register" element={<Login />} />
       </Routes>
     </div>
   );
---
+++ src/Website/Auth/Register/Login.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import "./Login.css";
+import logo from "../../../images/register-logo.svg";
+import { Link } from "react-router-dom";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+import { IoEyeOffOutline } from "react-icons/io5";
+import google from '../../../images/google.svg'
+import facebook from '../../../images/facebook.svg'
+const Login = () => {
+  const [hidePass, setHidePass] = useState(true);
+  return (
+    <div className="Login">
+      <div className="main">
+        <form className="form-box">
+          <h2>Welcome back</h2>
+          <p>Enter your email and password to login to your account.</p>
+          <div className="inputs">
+            <div className="input">
+              <input type="text" placeholder="Email Address" />
+            </div>
+            <div className="input">
+              <input
+                type={hidePass ? "password" : "text"}
+                placeholder="Password"
+              />
+              {hidePass ? (
+                <IoEyeOffOutline
+                  className="icon"
+                  onClick={() => setHidePass((prev) => !prev)}
+                />
+              ) : (
+                <MdOutlineRemoveRedEye
+                  className="icon"
+                  onClick={() => setHidePass((prev) => !prev)}
+                />
+              )}
+            </div>
+          </div>
+          <div className="options">
+            <div className="flex">
+              <input type="checkbox" name="remmember" id="remmember" />
+              <label htmlFor="remmember">Remember me</label>
+            </div>
+            <Link className="link">Forget password</Link>
+          </div>
+          <button type="submit">Log in</button>
+          <p className="seperator">- Or log in with -</p>
+          <div className="providers">
+            <Link className="link">
+              <img src={google} alt="google" loading="lazy" />
+              <span>Google</span></Link>
+            <Link className="link">
+              <img src={facebook} alt="facebook" loading="lazy" />
+              <span>Facebook</span></Link>
+          </div>
+          <p className="redirect">
+          Don't have an account? <Link className="link">sign up</Link> 
+          </p>
+        </form>
+
+
+        <div className="content">
+          <img src={logo} alt="juba" loading="lazy" />
+          <h2>
+            Welcome to <br />
+            Juba academy
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Condimentum aliquam sagittis
+            senectus purus fames.{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
