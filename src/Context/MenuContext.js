ebc9553ac37591391709157536c9abd24052f623
remon <remosamir443@gmail.com>
2025-04-04 07:11:46 -0700

    first upload

--- src/Context/MenuContext.js
+++ src/Context/MenuContext.js
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from 'react';
 export const Menu = createContext()
 const MenuContext = ({children}) => {
-  const [isOpen  , setIsOpen] = useState(true)
+  const [isOpen  , setIsOpen] = useState(false)
   return <Menu.Provider value={{isOpen ,setIsOpen }}>{children}</Menu.Provider>
 
 
