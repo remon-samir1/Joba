f2bf88edd343d75a1ad9a6e1657fecdefa9a3759
remon <remosamir443@gmail.com>
2025-04-04 10:52:07 -0700

    first upload

--- src/Dashboard/Admin/Courses/Courses.js
+++ src/Dashboard/Admin/Courses/Courses.js
@@ -25,7 +25,7 @@ const Courses = () => {
       <SelectBox title='Statue'/>
       <SelectBox title='Approval statue'/>
       </div>
-      <div className="table my-8 bg-white">
+      {/* <div className="table my-8 bg-white">
         <div className="flex justify-between mb-4 items-center border-b p-4  border-borderColor">
           <h4 className="text-main text-base ">Courses</h4>
           <Link className="flex justify-between gap-2 items-center text-white bg-main py-2 px-4 rounded-md link border border-main duration-500">
@@ -34,7 +34,7 @@ const Courses = () => {
           </Link>
         </div>
         <Table/>
-      </div>
+      </div> */}
     </div>
   );
 };
