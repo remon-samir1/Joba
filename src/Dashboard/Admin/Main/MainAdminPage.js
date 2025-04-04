cfddce8634856e6d52c84c60613e4e058c515e4b
remon <remosamir443@gmail.com>
2025-04-04 07:17:57 -0700

    first upload

--- src/Dashboard/Admin/Main/MainAdminPage.js
+++ src/Dashboard/Admin/Main/MainAdminPage.js
@@ -12,36 +12,36 @@ import { WindowSize } from "../../../Context/WindowSizeContext";
 
 const MainAdminPage = () => {
   const menu = useContext(WindowSize)
-  console.log(menu);
-  // return (
-    // <div className="MainAdminPage ">
-    //   <h3 className="font-bold text-textColor text-xl">Dashboard</h3>
-    //   <div className="mt-4 flex flex-wrap gap-5">
-    //     {MainAdminPageBoxData.map((data, index) => (
-    //       <MainAdminPageBox
-    //         key={index}
-    //         icon={data.icon}
-    //         title={data.title}
-    //         count={data.count}
-    //       />
-    //     ))}
-    //   </div>
-    //   <div className="charts flex mt-8 gap-4">
-    //     <div className="line flex-1 h-96">
-    //       <CustomLineChart />
-    //     </div>
-    //     <div className="pie ">
-    //       <CustomPieChart />
-    //     </div>
-    //   </div>
+  
+  return (
+    <div className="MainAdminPage ">
+      <h3 className="font-bold text-textColor text-xl">Dashboard</h3>
+      <div className="mt-4 flex flex-wrap gap-5">
+        {MainAdminPageBoxData.map((data, index) => (
+          <MainAdminPageBox
+            key={index}
+            icon={data.icon}
+            title={data.title}
+            count={data.count}
+          />
+        ))}
+      </div>
+      {/* <div className="charts flex mt-8 gap-4 flex-wrap">
+        <div className="line flex-1 h-96">
+          <CustomLineChart />
+        </div>
+        <div className="pie ">
+          <CustomPieChart />
+        </div>
+      </div> */}
 
-    //   <div className="my-8 flex justify-center items-center gap-3 ">
-    //     <Recents />
-    //     <Recents />
-    //     <Recents />
-    //   </div>
-    // </div>
-  // );
+      {/* <div className="my-8 flex justify-center items-center gap-3 ">
+        <Recents />
+        <Recents />
+        <Recents />
+      </div> */}
+    </div>
+  );
 };
 
 export default MainAdminPage;
